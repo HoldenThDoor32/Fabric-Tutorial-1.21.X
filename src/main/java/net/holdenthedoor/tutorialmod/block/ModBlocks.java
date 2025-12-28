e1c0515c147a317fc8b0f443dd9b19c1fac12931
Holden Wagner <holdoon032@gmail.com>
2025-12-27 22:16:19 -0600

    Added all the garnet blocks, as well as their functionalities.

---
+++ src/main/java/net/holdenthedoor/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,88 @@
+package net.holdenthedoor.tutorialmod.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.holdenthedoor.tutorialmod.TutorialMod;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
+import net.minecraft.block.MapColor;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+
+public class ModBlocks {
+
+    public static final Block PINK_GARNET_BLOCK = registerBlock(
+            "pink_garnet_block",
+            new Block(AbstractBlock.Settings.create()
+                    .mapColor(MapColor.PINK)
+                    .strength(3.0F, 3.0F)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.AMETHYST_BLOCK)
+            )
+    );
+
+    public static final Block RAW_PINK_GARNET_BLOCK = registerBlock(
+            "raw_pink_garnet_block",
+            new Block(AbstractBlock.Settings.create()
+                    .mapColor(MapColor.PINK)
+                    .strength(3.0F, 3.0F)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.AMETHYST_BLOCK)
+            )
+    );
+
+    public static final Block PINK_GARNET_ORE = registerBlock(
+            "pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2,5),
+                    AbstractBlock.Settings.create()
+                    .mapColor(MapColor.GRAY)
+                    .strength(3.0F, 3.0F)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE)
+            )
+    );
+
+    public static final Block DEEPSLATE_PINK_GARNET_ORE = registerBlock(
+            "deepslate_pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3,6),
+                    AbstractBlock.Settings.create()
+                            .mapColor(MapColor.GRAY)
+                            .strength(3.5F, 3.5F)
+                            .requiresTool()
+                            .sounds(BlockSoundGroup.DEEPSLATE)
+            )
+    );
+
+
+
+    //registers the block into the system
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(TutorialMod.MOD_ID, name), block);
+    }
+
+    //registers the block in item form into the system
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        TutorialMod.LOGGER.info("Registering Mod Blocks for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModBlocks.PINK_GARNET_BLOCK);
+            entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+            entries.add(ModBlocks.PINK_GARNET_ORE);
+            entries.add(ModBlocks.DEEPSLATE_PINK_GARNET_ORE);
+        });
+
+    }
+
+}

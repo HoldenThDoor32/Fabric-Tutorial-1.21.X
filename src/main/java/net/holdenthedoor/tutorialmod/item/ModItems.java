e1bdd0dc7e9256ca44eeb43ede4368a7266bc3be
Holden Wagner <holdoon032@gmail.com>
2025-12-28 08:49:24 -0600

    Added sorting to foods in the creative menu.

--- src/main/java/net/holdenthedoor/tutorialmod/item/ModItems.java
+++ src/main/java/net/holdenthedoor/tutorialmod/item/ModItems.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.holdenthedoor.tutorialmod.TutorialMod;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -26,8 +27,13 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
-            entries.add(CAULIFLOWER);
-            entries.add(EMERALD_APPLE);
         });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            entries.addAfter(Items.CARROT, CAULIFLOWER);
+            entries.addAfter(Items.GOLDEN_APPLE, EMERALD_APPLE);
+        });
+
+
     }
 }
